a477bf647865ae9f76ff67fb29b0e014daeea1d7
John Roper <johnroper100@gmail.com>
2017-02-19 14:13:09 -0500

    required files

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const url = require('url')
 let win
 
 function createWindow () {
-  win = new BrowserWindow({width: 800, height: 600, title: "ProjectMaster"})
+  win = new BrowserWindow({width: 1050, height: 600, title: "ProjectMaster"})
 
   win.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
@@ -14,7 +14,7 @@ function createWindow () {
   }))
 
   // Open the DevTools.
-  win.webContents.openDevTools()
+  //win.webContents.openDevTools()
 
   win.on('closed', () => {
     win = null
